1b69bd0d728eb3705e3701455a7468d0fa0c1003
Annapureddy Srinivas Reddy <digitech1993@gmail.com>
2022-04-26 16:18:16 +0530

    first commit

---
+++ my-maven-app/src/main/java/com/sapient/endur/ui/App.java
@@ -0,0 +1,8 @@
+package com.sapient.endur.ui;
+
+
+public class App {
+    public static void main( String[] args )    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ my-maven-app/src/test/java/com/sapient/endur/ui/AppTest.java
@@ -0,0 +1,10 @@
+package com.sapient.endur.ui;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {   
+ 
+    public void testApp()    {
+        assertTrue( true );
+    }
+}
